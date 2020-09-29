7f56720c9692e3d0f2d38b7ea5b7a2bc7e5e415f
LudovicCocquyt <ludlud59@hotmail.fr>
2020-09-30 00:18:36 +0200

    creation page presentation & page news

--- LT_Project/src/Controller/HomeController.php
+++ LT_Project/src/Controller/HomeController.php
@@ -6,17 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ContentStaticRepository;
+use App\Repository\NewsRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="index_page")
      */
-    public function home(ContentStaticRepository $ContentStaticRepo)
+    public function home(ContentStaticRepository $ContentStaticRepo,NewsRepository $newsRepo)
     {
 
     	return $this->render('homePage.html.twig', [
-            'statics' => $ContentStaticRepo->findAll()
+			'news'    => $newsRepo->findAll(),
+			'statics' => $ContentStaticRepo->findAll()
         ]);
 	}
 }
