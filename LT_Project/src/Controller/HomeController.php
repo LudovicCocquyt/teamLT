af3134318888bbd7b3bb7c52f29caff44ff47e63
LudovicCocquyt <ludlud59@hotmail.fr>
2020-10-23 23:48:06 +0200

    maj img news home page

--- LT_Project/src/Controller/HomeController.php
+++ LT_Project/src/Controller/HomeController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ContentStaticRepository;
-use App\Repository\PalmaresRepository;
 use App\Repository\ResultatsRepository;
+use App\Repository\PalmaresRepository;
+use App\Repository\ImagesRepository;
 use App\Repository\JeuxRepository;
 use App\Repository\NewsRepository;
 
@@ -16,15 +17,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index_page")
      */
-    public function home(ContentStaticRepository $ContentStaticRepo,NewsRepository $newsRepo, JeuxRepository $jeuxRepo, PalmaresRepository $PalmaresRepo, ResultatsRepository $resultatsRepo)
+    public function home(ContentStaticRepository $contentStaticRepo,NewsRepository $newsRepo, JeuxRepository $jeuxRepo, PalmaresRepository $palmaresRepo, ResultatsRepository $resultatsRepo, ImagesRepository $imageRepo)
     {
+        $idLastNews = $newsRepo->findBy(array(),array('id' => 'DESC'))[0]->getId();
 
     	return $this->render('homePage.html.twig', [
-            'jeux'     => $jeuxRepo->findBy(array('isActive' => true)),
-            'news'     => $newsRepo->findAll(),
-            'statics'  => $ContentStaticRepo->findAll(),
+            'jeux'      => $jeuxRepo->findBy(array('isActive' => true)),
+            'news'      => $newsRepo->findAll(),
+            'statics'   => $contentStaticRepo->findAll(),
             'resultats' => $resultatsRepo->findAll(),
-            'palmares' => $PalmaresRepo->findAll()
+            'palmares'  => $palmaresRepo->findAll(),
+            'imageNews' => $imageRepo->findBy( array('news' => $idLastNews))[0]->getName()
         ]);
 	}
 }
