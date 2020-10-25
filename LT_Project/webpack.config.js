f1b7300a834b48eaf506e48a9a34d17d78b23542
LudovicCocquyt <ludlud59@hotmail.fr>
2020-10-25 12:16:23 +0100

    creation de plusieurs fichiers CSS && supp balise de style

--- LT_Project/webpack.config.js
+++ LT_Project/webpack.config.js
@@ -24,6 +24,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('admin', './assets/admin.js')
+    .addEntry('user-show', './assets/user-show.js')
+    .addEntry('lineup-show', './assets/lineup-show.js')
+
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
